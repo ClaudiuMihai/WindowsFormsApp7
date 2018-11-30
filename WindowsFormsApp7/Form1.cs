74e8b7fbb97ffc04a7cae9d0f6f5c790ecd28d16
home <home@HOME-PC>
2018-11-30 16:18:24 +0200

    PrimeNumbers

--- WindowsFormsApp7/Form1.cs
+++ WindowsFormsApp7/Form1.cs
@@ -16,5 +16,59 @@ namespace WindowsFormsApp7
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double i, ok = 1;
+            int n = 0;
+            n = Convert.ToInt32(textBox1.Text);
+            for (i = 2; i <= n / 2; i++)
+
+            {
+                if (n % i == 0)
+                    ok = 0;
+            }
+            if (n <= 1)
+            {
+                ok = 0;
+            }
+        
+           
+            if (ok == 1)
+            {
+                BackColor = Color.Green;
+            }
+            else
+            {
+                BackColor = Color.Red;
+            }
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char x = e.KeyChar;
+            if (!char.IsDigit(x) && x != 8 && x != 46)
+            {
+                e.Handled = true;
+                MessageBox.Show("salut,eu sunt Vasilica");
+                
+            }
+                    
+                
+
+        }
     }
 }
